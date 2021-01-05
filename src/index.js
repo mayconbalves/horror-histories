03cfdf0d68c650b71b710e839d41c050f8510b97
mayconbalves <mayconmustaine@hotmail.com>
2021-01-04 22:40:31 -0300

    fix(*): eslint errors

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { createGlobalStyle} from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
   * {
